10e0c0224068bdcff6579c3dbfd3f553b235a0f8
AurelijaMaz <aurelija.mazeikiene@gmail.com>
2021-03-31 10:49:09 +0300

    feat: downl. mongoose, connect in dataBase mongoDB compass

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config({ path:'server/.env'});
 
 
 const  server = express();
 
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT,
+    MG_COMPASS_USER,
+    MG_COMPASS_PASS,
+    MG_COMPASS_CLUSTER_URL,
+    MG_COMPASS_CLUSTER_DB } = process.env;
 
 //Midlewares
 server.use(cors());
@@ -17,6 +22,18 @@ server.get('/', (req, res) => {
 })
 
 
-server.listen(SERVER_PORT, () => {
-    console.log(`Users server is running on http://localhost:${SERVER_PORT}`);
-});
+
+
+
+const db = mongoose.connection;
+mongoose.connect(`mongodb+srv://${MG_COMPASS_USER}:${MG_COMPASS_PASS}@${MG_COMPASS_CLUSTER_URL}/${MG_COMPASS_CLUSTER_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true} )
+
+db.on('connected', function () {
+    console.log('Connected to Database successfuly')
+    server.listen(SERVER_PORT, () => {
+        console.log(`Users server is running on http://localhost:${SERVER_PORT}`);
+    });
+
+})
+
+db.on('error', (error) => console.error('FAILED TO CONNECT TO DB:\n ' + error))
