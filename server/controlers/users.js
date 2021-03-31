5d065071c4cecacdae996e0426f809ec959e7077
AurelijaMaz <aurelija.mazeikiene@gmail.com>
2021-03-31 11:14:45 +0300

    feat:  created user.js in controlers, UserMedel.js in models

---
+++ server/controlers/users.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const UserModel = require('../models/UserModel');
+
+const getUsers =  async (req, res) => {
+res.status(200).json('')
+}
+
+const postUser =  async (req, res) => {
+    res.status(200).json('')
+}
+
+const updateUser =  async (req, res) => {
+    res.status(200).json('')
+}
+
+const deleteUser =  async (req, res) => {
+    res.status(200).json('')
+}
+
+module.exports = {
+
+    getUsers,
+    postUser,
+    updateUser,
+    deleteUser
+}
